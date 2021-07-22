e1762dacb833fd5ddcdec65e592a70095261254a
SohilaBogdadyNagdy <sohila.bogdady2012@gmail.com>
2021-07-23 00:39:10 +0200

    Add controllers

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 #end
 Rails.application.routes.draw do
-  get 'articles'
-  post 'articles'
-  patch 'articles/publish'
-  patch 'articles/approve'
-  delete 'articles/id'
+  #get 'articles'
+  #post 'articles'
+  #patch 'articles/publish'
+  #patch 'articles/approve'
+  #delete 'articles/id'
 end
