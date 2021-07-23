01dd5fe646d65b519b80a1a0af6eda6e295a36a4
SohilaBogdadyNagdy <sohila.bogdady2012@gmail.com>
2021-07-23 22:14:07 +0200

    Define controller methods

--- app/controllers/article_controller.rb
+++ app/controllers/article_controller.rb
@@ -1,2 +1,25 @@
 class ArticleController < ApplicationController
+    def list
+        @articles = Article.all
+    end
+    
+    def show
+    end
+    
+    def new
+        @article = Article.new
+    end
+    
+    def create
+    end
+    
+    def edit
+    end
+    
+    def update
+    end
+    
+    def delete
+    end
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,12 @@
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 #end
 Rails.application.routes.draw do
-  #get 'articles'
-  #post 'articles'
-  #patch 'articles/publish'
-  #patch 'articles/approve'
-  #delete 'articles/id'
+  get '/article/list'
+  #post 'articles', to: 'articles#index'
+  #patch 'articles/publish', to: 'articles#index'
+  #patch 'articles/approve', to: 'articles#index'
+  #delete 'articles/id', to: 'articles#index'
+  #get '/articles/:id', to: 'articles#show'
+
+
 end
